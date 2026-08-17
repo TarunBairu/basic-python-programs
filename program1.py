21e01c8bdc64b5429e21dfc0479ab970a84c1f8c
TarunBairu <bairutarun357@gmail.com>
2026-08-18 01:23:28 +0530

    Third version

--- program1.py
+++ program1.py
@@ -8,7 +8,7 @@ client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 prompt = """
 Write a professional leave request email
-for one days leave.
+for eight days leave.
 """
 
 response = client.chat.completions.create(
